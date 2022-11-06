371f4ebb0a227074259e2db67f63cc6a166931fd
sumitkahar992 <sumitkahar073@gmail.com>
2022-11-06 18:13:00 +0530

    Accompanist and Hilt Navigation Dependencies Addded.

--- app/src/main/java/com/example/paging_app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/paging_app/presentation/MainActivity.kt
@@ -4,29 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import com.example.paging_app.presentation.theme.PAGING_APPTheme
+import com.example.paging_app.presentation.newsui.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    
+
     private val viewModel by viewModels<UserViewModel>()
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PAGING_APPTheme() {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ){
-                    UserHome(viewModel = viewModel)
-                }
-            }
+
+            HomeScreen()
+
         }
     }
 }
